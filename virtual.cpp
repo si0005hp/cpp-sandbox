ff8b6ba19cec0a50cdf07523274ae3a5f3f02e53
si0005hp <si0005hp@gmail.com>
2021-02-14 20:57:17 +0900

    virtual

---
+++ virtual.cpp
@@ -0,0 +1,63 @@
+#include "all.h"
+
+using namespace std;
+
+namespace Virtual
+{
+
+using namespace std;
+
+class BasePrinter
+{
+  public:
+    void Print(const string &msg)
+    {
+        PrintLine();
+        cout << msg << endl;
+        PrintLine();
+    }
+
+  protected:
+    virtual void PrintLine()
+    {
+        throw "Not supposed to be called.";
+    }
+};
+
+class Printer1 : public BasePrinter
+{
+  protected:
+    void PrintLine() override
+    {
+        cout << "*******************************" << endl;
+    }
+};
+
+class Printer2 : public BasePrinter
+{
+  protected:
+    void PrintLine() override
+    {
+        cout << "================================" << endl;
+    }
+};
+
+void run()
+{
+    // BasePrinter p1 = Printer1();
+    // BasePrinter p2 = Printer2();
+
+    BasePrinter *p1 = new Printer1();
+    BasePrinter *p2 = new Printer2();
+
+    p1->Print("msg by Printer1");
+    p2->Print("msg by Printer2");
+}
+
+} // namespace Virtual
+
+int main(int argc, char const *argv[])
+{
+    Virtual::run();
+    return 0;
+}
