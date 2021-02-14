35db59c9c11113407bc228d9f842914e71be4378
si0005hp <si0005hp@gmail.com>
2021-02-14 21:25:05 +0900

    string

---
+++ string.cpp
@@ -0,0 +1,35 @@
+#include "all.h"
+
+using namespace std;
+
+namespace String
+{
+
+class Foo
+{
+  public:
+    Foo(const string &msg) : mMsg(msg)
+    {
+    }
+    const string mMsg;
+};
+
+void run()
+{
+    string s1 = "abc";
+    cout << s1 + " def" << endl;
+
+    Foo("a");
+    Foo(s1 + "a");
+    // Foo("a" + "a"); NG
+    Foo(string("a") + "a");
+    Foo("a" + string("a"));
+}
+
+} // namespace String
+
+int main(int argc, char const *argv[])
+{
+    String::run();
+    return 0;
+}
