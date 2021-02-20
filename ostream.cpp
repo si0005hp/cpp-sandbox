562e56626d43e4e3eaf45e035d7db496361f0784
si0005hp <si0005hp@gmail.com>
2021-02-21 02:18:59 +0900

    ostream

---
+++ ostream.cpp
@@ -0,0 +1,48 @@
+#include "all.h"
+
+using namespace std;
+
+namespace Ostream
+{
+
+class Writer
+{
+  public:
+    Writer(std::ostream &os) : mOs(os)
+    {
+    }
+    void Write(const string &s)
+    {
+        mOs << s;
+    }
+    void Writeln(const string &s)
+    {
+        mOs << s << endl;
+    }
+
+    std::ostream &mOs;
+};
+
+void run()
+{
+    Writer w(std::cout);
+    w.Write("aaa");
+    w.Writeln("bbb");
+    w.Writeln("12345");
+
+    std::ostringstream oss;
+    Writer w2(oss);
+    w2.Write("ccc");
+    w2.Writeln("ddd");
+    w2.Writeln("999");
+
+    cout << oss.str() << endl;
+}
+
+} // namespace Ostream
+
+int main(int argc, char const *argv[])
+{
+    Ostream::run();
+    return 0;
+}
