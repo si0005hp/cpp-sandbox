63362d4dc3c82733cebb746daa9336b89b72fcda
si0005hp <si0005hp@gmail.com>
2021-02-27 20:59:46 +0900

    free

---
+++ free.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct
+{
+    int id;
+    char *name;
+} Item;
+
+typedef struct
+{
+    int status;
+    Item *item;
+} Bucket;
+
+void printItem(Item *item)
+{
+    printf("%s: %d\n", item->name, item->id);
+}
+
+Bucket *makeBucket(int id, char *name)
+{
+    Item *item = malloc(sizeof(Item));
+    item->id = id;
+    item->name = name;
+
+    Bucket *bucket = malloc(sizeof(Bucket));
+    bucket->status = 0;
+    bucket->item = item;
+
+    return bucket;
+}
+
+void freeBucket(Bucket *b)
+{
+    free(b->item);
+    free(b);
+}
+
+int main(int argc, char const *argv[])
+{
+    unsigned long counter = 0;
+    while (1)
+    {
+        Bucket *b =
+            makeBucket(0, "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                          "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                          "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                          "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                          "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                          "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery long string");
+        freeBucket(b);
+
+        counter++;
+        if (counter % 10000 == 0)
+            sleep(1);
+    }
+    return 0;
+}
---
+++ free.cpp
@@ -0,0 +1,73 @@
+#include "all.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+namespace Free
+{
+
+typedef struct
+{
+    int id;
+    char *name;
+} Item;
+
+typedef struct
+{
+    int status;
+    Item *item;
+} Bucket;
+
+void printItem(Item *item)
+{
+    printf("%s: %d\n", item->name, item->id);
+}
+
+Bucket *makeBucket(int id, char *name)
+{
+    Item *item = new Item();
+    item->id = id;
+    item->name = name;
+
+    Bucket *bucket = new Bucket();
+    bucket->status = 0;
+    bucket->item = item;
+
+    return bucket;
+}
+
+void freeBucket(Bucket *b)
+{
+    delete b->item;
+    delete b;
+}
+
+void run()
+{
+    unsigned long counter = 0;
+    while (1)
+    {
+        Bucket *b = makeBucket(
+            0, (char *)"veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                       "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                       "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                       "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                       "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery"
+                       "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryvery long string");
+        freeBucket(b);
+
+        counter++;
+        if (counter % 10000 == 0)
+            sleep(1);
+    }
+}
+
+} // namespace Free
+
+int main(int argc, char const *argv[])
+{
+    Free::run();
+    return 0;
+}
