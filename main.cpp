d1ee27807e033194db4bc31a3b4c4928c72fe593
si0005hp <si0005hp@gmail.com>
2021-03-06 05:06:12 +0900

    template2

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    vector<string> v{"1", "2"};
-    for (auto s : v)
-    {
-        cout << s << endl;
-    }
+    // vector<string> v{"1", "2"};
+    // for (auto s : v)
+    // {
+    //     cout << s << endl;
+    // }
+
+    std::cout << std::setfill('0') << std::setw(4) << 12;
     return 0;
 }
--- template.cpp
+++ template.cpp
@@ -52,8 +52,8 @@ T DynamicCast(Value &value)
 void run()
 {
     // String s("a");
-    Value *v = new String("a");
-    v->As<string>();
+    // Value *v = new String("a");
+    // v->As<string>();
     // String &s2 = dynamic_cast<String &>(v);
 
     // Value v2 = Number(12);
@@ -62,10 +62,68 @@ void run()
     // DynamicCast(v2);
 }
 
+namespace Generic
+{
+
+class IPrintable
+{
+  public:
+    virtual ~IPrintable() {}
+    virtual void print() const = 0;
+};
+
+template <class T>
+class Array
+{
+  public:
+    ~Array()
+    {
+        for (auto &&p : v_)
+            delete p;
+    }
+
+    void append(T *item) { v_.push_back(item); }
+    void printAll() const
+    {
+        for (auto &&p : v_)
+            p->print();
+    }
+
+  private:
+    vector<IPrintable *> v_;
+};
+
+class Item : public IPrintable
+{
+  public:
+    Item(const string &name) : name_(name) {}
+    ~Item() { cout << "Destructor of " << name_ << " is called." << endl; }
+
+    void print() const override { cout << name_ << endl; }
+
+    string name_;
+};
+
+class ItemArray : public Array<Item>
+{
+};
+
+void run()
+{
+    // Array<Item> items;
+    ItemArray items;
+
+    items.append(new Item("aaa"));
+    items.append(new Item("bbb"));
+    items.printAll();
+}
+
+} // namespace Generic
+
 } // namespace Template
 
 int main(int argc, char const *argv[])
 {
-    Template::run();
+    Template::Generic::run();
     return 0;
 }
